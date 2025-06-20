0e493369930acc73fba5dce40cb5f49112c59a31
RiverMelanie <RiverMelanie@noreply.gitcode.com>
2025-06-20 19:48:01 +0800

    Initialize Spring Boot project

---
+++ src/main/java/com/neucamp/testalliancehubbackend/TestAllianceHubBackendApplication.java
@@ -0,0 +1,14 @@
+package com.neucamp.testalliancehubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAllianceHubBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestAllianceHubBackendApplication.class, args);
+        System.out.println("TestAllianceHubBackendApplication started");
+    }
+
+}
---
+++ src/test/java/com/neucamp/testalliancehubbackend/TestAllianceHubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neucamp.testalliancehubbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAllianceHubBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
