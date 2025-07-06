ec69e75661a933cb3f6c974a4ade06010e89f489
Y-Z-L <2918386159@qq.com>
2025-07-06 23:15:41 +0800

    增加了移动端的动态和浏览记录

--- src/main/java/com/neucamp/testalliancehubbackend/mapper/MobileIndustryMapper.java
+++ src/main/java/com/neucamp/testalliancehubbackend/mapper/MobileIndustryMapper.java
@@ -1,6 +1,10 @@
 package com.neucamp.testalliancehubbackend.mapper;
 
+import com.neucamp.testalliancehubbackend.entity.IndustryDynamic;
+import com.neucamp.testalliancehubbackend.entity.Visit;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -23,4 +27,19 @@ public interface MobileIndustryMapper {
     // 调试用：统计总访问量
     @Select("SELECT COUNT(*) AS totalVisits FROM mobile_dynamic_visit")
     long getTotalVisitCount();
+
+    @Select("SELECT * FROM industry_dynamic " +
+            "WHERE (title LIKE CONCAT('%',#{keyword},'%') OR content LIKE CONCAT('%',#{keyword},'%')) " +
+            "AND auditStatus = 1 " +
+            "ORDER BY createTime DESC " )
+    List<IndustryDynamic> getAllDynamicsByKeyword(@Param("keyword") String keyword);
+
+    @Insert("INSERT INTO mobile_dynamic_visit (user_id, dynamic_id, visit_time) " +
+            "VALUES (#{user_id}, #{dynamic_id}, #{visit_time})")
+    int insertVisit(Visit visit);
+
+    @Select("SELECT COUNT(*) FROM mobile_dynamic_visit " +
+            "WHERE user_id = #{user_id} AND dynamic_id = #{dynamic_id}")
+    int checkVisitExists(@Param("user_id") Integer user_id,
+                         @Param("dynamic_id") Integer dynamic_id);
 }
